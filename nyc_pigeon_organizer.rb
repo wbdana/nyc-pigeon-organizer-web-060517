f9303bf82979fdf1e3a101548fe2195c56b9dd2f
William Dana <william.b.dana@gmail.com>
2017-05-23 13:47:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,41 @@
+require 'pry'
+
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, info_hash|
+    info_hash.each do |phenotype, name_arr|
+      name_arr.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][attribute]
+          pigeon_list[name].merge!({attribute => [phenotype.to_s]})
+        else
+          pigeon_list[name][attribute].push(phenotype.to_s)
+        end
+      end
+    end
+  end
+  pigeon_list
 end
+
+nyc_pigeon_organizer(data)
