ebb4e775ec5bc5761a5730875996ca222c588a18
wbdana <william.b.dana@gmail.com>
2017-06-06 12:06:41 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-data = {
+pigeon_data = {
   :color => {
     :purple => ["Theo", "Peter Jr.", "Lucky"],
     :grey => ["Theo", "Peter Jr.", "Ms. K"],
@@ -19,23 +19,43 @@ data = {
   }
 }
 
-def nyc_pigeon_organizer(data)
+#def nyc_pigeon_organizer(data)
+#   pigeon_list = {}
+#   data.each do |attribute, info_hash|
+#     info_hash.each do |phenotype, name_arr|
+#       name_arr.each do |name|
+#         if !pigeon_list[name]
+#           pigeon_list[name] = {}
+#         end
+#         if !pigeon_list[name][attribute]
+#           pigeon_list[name].merge!({attribute => [phenotype.to_s]})
+#         else
+#           pigeon_list[name][attribute].push(phenotype.to_s)
+#         end
+#       end
+#     end
+#   end
+#   pigeon_list
+# end
+#
+# nyc_pigeon_organizer(data)
+
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_names = []
   pigeon_list = {}
-  data.each do |attribute, info_hash|
-    info_hash.each do |phenotype, name_arr|
-      name_arr.each do |name|
-        if !pigeon_list[name]
-          pigeon_list[name] = {}
-        end
-        if !pigeon_list[name][attribute]
-          pigeon_list[name].merge!({attribute => [phenotype.to_s]})
-        else
-          pigeon_list[name][attribute].push(phenotype.to_s)
+  pigeon_names << pigeon_data[:gender].values
+  pigeon_names = pigeon_names.flatten
+  pigeon_names.each{|name| pigeon_list[name] = {}}
+  pigeon_list.each do |name, chars|
+    pigeon_data.each do |data_kind, char_hash|
+      pigeon_list[name][data_kind] = []
+      char_hash.each do |pheno, names_with_pheno|
+        if names_with_pheno.include?(name)
+          pigeon_list[name][data_kind] << pheno.to_s
         end
       end
     end
   end
   pigeon_list
 end
-
-nyc_pigeon_organizer(data)
